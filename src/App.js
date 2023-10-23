82d5a09d0e6f3927fbcbe1f24893efbda54a18d7
OsmanW <osmanwako411@gmail.com>
2023-10-23 11:55:34 +0300

    Add all first file

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import './App.css';
+
+function App() {
+  return <div className='app'>This is our First Class</div>;
+}
+
+export default App;
---
+++ src/components/main.js
@@ -0,0 +1,6 @@
+import './main.css';
+function Main() {
+  return <div className='main'>This is our First main Class</div>;
+}
+
+export default Main;
